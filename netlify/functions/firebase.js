ce38ec1f0f839bfda52952d8b42bddde050d33d8
Patrick Dorion <patrick.dorion@kellogg.nothwestern.edu>
2021-05-16 19:54:14 -0500

    v3

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyDlDuIoQu2gOdSJ9g8FkiefxNQdrzRzszs",
+authDomain: "kiei-451-954b2.firebaseapp.com",
+projectId: "kiei-451-954b2",
+storageBucket: "kiei-451-954b2.appspot.com",
+messagingSenderId: "331089885613",
+appId: "1:331089885613:web:52bbe00e052048c04cc924"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
